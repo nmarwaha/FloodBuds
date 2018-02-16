a71aed6cfcaa494060541f9e5c82814ba44e6028
jemedina17 <36553790+jemedina17@users.noreply.github.com>
2018-02-16 15:38:16 -0800

    Add files via upload

---
+++ IRproximity.ino
@@ -0,0 +1,16 @@
+//collects data from an analog sensor
+
+int sensorpin = 0;                 // analog pin used to connect the sharp sensor
+int val = 0;                 // variable to store the values from sensor(initially zero)
+
+void setup()
+{
+  Serial.begin(9600);               // starts the serial monitor
+}
+ 
+void loop()
+{
+  val = analogRead(sensorpin);       // reads the value of the sharp sensor
+  Serial.println(val);            // prints the value of the sensor to the serial monitor
+  delay(400);                    // wait for this much time before printing next value
+}
